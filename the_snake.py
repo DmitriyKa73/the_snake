237c3a7d5ccda6d18e5c1ebe02e2a18cb8ea9ace
DmitriyKa73 <109173727+DmitriyKa73@users.noreply.github.com>
2024-05-09 21:01:32 +0400

    Update the_snake.py

--- the_snake.py
+++ the_snake.py
@@ -29,7 +29,7 @@ APPLE_COLOR = (255, 0, 0)
 SNAKE_COLOR = (0, 255, 0)
 
 # Скорость движения змейки:
-SPEED = 20
+SPEED = 15
 
 # Настройка игрового окна:
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
